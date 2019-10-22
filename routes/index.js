b153fc26778adae701399c71bea033e0b6c5af0a
Paul Dalais <17314824@vcconnect.co.za>
2019-10-22 17:25:13 +0200

    no reports

--- routes/index.js
+++ routes/index.js
@@ -48,11 +48,6 @@ router.get('/horse/:horseID', function(req, res) {
 
 
 
-
-
-
-
-
 // Add Horse Page
 router.get('/add-horse', (req, res) => {
 	res.render('add-horse', {title: 'Add Horse'});
@@ -80,9 +75,6 @@ router.post('/add-horse', (req, res) => {
 router.get('/edit-horse', (req, res) => {
 	res.render('edit-horse', {title: 'Edit Horse'});
 });
-
-
-
 
 
 
@@ -131,20 +123,75 @@ router.post('/users', (req, res) => {
 
 
 
-
-
-
-
 // Reports Page
 router.get('/reports', (req, res) => {
-	res.render('reports', {title: 'Reports'});
+	
+
+/*	
+//var ctx = document.getElementById('myChart');
+var ctx = 'myChart';
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                   // callback: (value) => '$' + value
+                }
+            }]
+        }
+    }
 });
 
-
-
-
-
-
+const chartCallback = (ChartJS) => {
+ 
+    // Global config example: https://www.chartjs.org/docs/latest/configuration/
+    ChartJS.defaults.global.elements.rectangle.borderWidth = 2;
+    // Global plugin example: https://www.chartjs.org/docs/latest/developers/plugins.html
+    ChartJS.plugins.register({
+        // plugin implementation
+    });
+    // New chart type example: https://www.chartjs.org/docs/latest/developers/charts.html
+    ChartJS.controllers.MyType = ChartJS.DatasetController.extend({
+        // chart implementation
+    });
+};
+ */
+/*
+(async () => {
+    const canvasRenderService = new CanvasRenderService(width, height, chartCallback);
+    const image = await canvasRenderService.renderToBuffer(configuration);
+    const dataUrl = await canvasRenderService.renderToDataURL(configuration);
+    const stream = canvasRenderService.renderToStream(configuration);
+})();
+	*/
+	res.render('reports', {title: 'Reports'});
+});
 
 
 // Login Page
